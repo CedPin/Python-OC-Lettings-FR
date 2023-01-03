2cf77c97407b8e86d3b993ece802107cad8e88e2
Cedric Pinson <a.cedric.pinson@gmail.com>
2023-01-03 15:42:33 +0100

    test static files

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
@@ -114,6 +115,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    "./static/",
+]
+
+
+django_heroku.settings(locals())
 
 
 sentry_sdk.init(
