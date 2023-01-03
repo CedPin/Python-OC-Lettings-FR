39417e2f8769e8e3c3705079bb9dae23f50c765a
Cedric Pinson <a.cedric.pinson@gmail.com>
2023-01-03 16:00:14 +0100

    test static files

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -118,9 +118,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    "./static/",
-]
+# STATICFILES_DIRS = [
+#     "./static/",
+# ]
 
 
 django_heroku.settings(locals())
